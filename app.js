fd849cdda4ba13720d1cf7ee9775b1fef5bdce03
n-shtetl <yomouschi@gmail.com>
2019-11-17 23:22:51 -0800

    Add routes

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose');
-
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => {
+    console.log(res);
+    res.send("Hello world");
+})
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`))
